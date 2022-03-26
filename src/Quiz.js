8afa2a4953faa083685677b1f3b63f0538f343b4
Callum Reid <callum.reid92@hotmail.com>
2022-03-26 11:12:29 +1100

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends React.Component {
     render() {
         return (
             <div>
-                <div className='QuizQuestion'>{this.state.quiz_questions(props.quizData)}, {instruction_text}</div>
+                <div className='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div>
             </div>
         )
     }
