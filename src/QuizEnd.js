d515327d1cfe2041440f117b3d8d80a775891844
Callum Reid <callum.reid92@hotmail.com>
2022-03-26 12:30:43 +1100

    Completed module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
-    redner() {
+    render() {
         <div>
             <p>Thanks for playing!</p>
             <a href=''>Reset Quiz</a>
